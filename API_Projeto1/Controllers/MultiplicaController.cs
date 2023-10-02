1483bd2420145661dcb0d1d31862d3bcbfb760e7
Luiz Martinho <luiz.martinho@grupoccaa.com.br>
2023-10-02 17:20:09 -0300

    Adicionar arquivos de projeto.

---
+++ API_Projeto1/Controllers/MultiplicaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using API_Projeto1.Models;
+
+namespace API_Projeto1.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class MultiplicaController : Controller
+    {
+        [HttpGet]
+        public ActionResult Get(string numero)
+        {
+            int num = Convert.ToInt32(numero);
+
+            return Ok(num * 2);
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody]POSTNumero numero)
+        {
+            int num = Convert.ToInt32(numero.Num);
+            numero.Num = (num * 2).ToString();
+            return Ok(numero.Num);
+        }
+    }
+}
---
+++ API_Projeto1/Models/POSTNumero.cs
@@ -0,0 +1,7 @@
+﻿namespace API_Projeto1.Models
+{
+    public class POSTNumero
+    {
+        public string Num { get; set; }
+    }
+}
